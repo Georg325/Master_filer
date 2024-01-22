ec4dd535a2c2768d552c3ee77979e972fb93ab5d
Georg325 <georg.vang@nmbu.no>
2024-01-22 10:54:52 +0100

    the base

--- funtion_file.py
+++ funtion_file.py
@@ -1,6 +1,9 @@
 import numpy as np
 import scipy as sp
 import tensorflow as tf
+from tensorflow.keras import backend as K
+import matplotlib.pyplot as plt
+import pandas as pd
 
 
 def matrix_maker(rows, cols=None, kernel_size=(2, 2), line_size=(1, 2), num_per_mat=3):
@@ -25,7 +28,7 @@ def matrix_maker(rows, cols=None, kernel_size=(2, 2), line_size=(1, 2), num_per_
 
         matrix_with_line = np.ones((rows, cols))
         matrix_with_line[line_start_position[0]:line_start_position[0] + line_size[0],
-                         line_start_position[1]:line_start_position[1] + line_size[1]] = alfa[i]
+        line_start_position[1]:line_start_position[1] + line_size[1]] = alfa[i]
 
         matrix_line_fade.append(smooth_matrix * matrix_with_line)
 
@@ -34,11 +37,50 @@ def matrix_maker(rows, cols=None, kernel_size=(2, 2), line_size=(1, 2), num_per_
 
         else:
             matrix_with_line[line_start_position[0]:line_start_position[0] + line_size[0],
-                             line_start_position[1]:line_start_position[1] + line_size[1]] = 0
+            line_start_position[1]:line_start_position[1] + line_size[1]] = 0
 
             line_pos_mat.append(np.logical_not(matrix_with_line).astype(int))
 
     return tf.convert_to_tensor(matrix_line_fade), tf.convert_to_tensor(line_pos_mat), tf.convert_to_tensor(alfa)
+
+
+def F1_score(y_true, y_pred):
+    TP = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    P = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = TP / (P + K.epsilon())
+
+    Pred_P = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = TP / (Pred_P + K.epsilon())
+    return 2 * ((precision * recall) / (precision + recall + K.epsilon()))
+
+
+def plot_training_history(training_history_object, list_of_metrics=None):
+    """
+    Input:
+        training_history_object:: Object returned by model.fit() function in keras
+        list_of_metrics        :: A list of metrics to be plotted. Use if you only
+                                  want to plot a subset of the total set of metrics
+                                  in the training history object. By Default it will
+                                  plot all of them in individual subplots.
+    """
+    history_dict = training_history_object.history
+    if list_of_metrics is None:
+        list_of_metrics = [key for key in list(history_dict.keys()) if 'val_' not in key]
+    trainHistDF = pd.DataFrame(history_dict)
+    # trainHistDF.head()
+    train_keys = list_of_metrics
+    valid_keys = ['val_' + key for key in train_keys]
+    nr_plots = len(train_keys)
+    fig, ax = plt.subplots(1,nr_plots,figsize=(5*nr_plots,4))
+    for i in range(len(train_keys)):
+        ax[i].plot(np.array(trainHistDF[train_keys[i]]), label='Training')
+        ax[i].plot(np.array(trainHistDF[valid_keys[i]]), label='Validation')
+        ax[i].set_xlabel('Epoch')
+        ax[i].set_title(train_keys[i])
+        ax[i].grid('on')
+        ax[i].legend()
+    fig.tight_layout()
+    plt.show()
 
 
 if __name__ == '__main__':
