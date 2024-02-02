b420d3a2a0f0b237e6cf3212c0ff9a219901b642
Georg325 <georg.vang@nmbu.no>
2024-02-02 12:45:16 +0100

    model saving

--- Master.py
+++ Master.py
@@ -1,11 +1,10 @@
-#%%
 import time
 from funtion_file import *
-#%%
+
 matrix_params = {
-    'mat_size': (5,5),
+    'mat_size': (5, 5),
     'kernel_size': (2, 2),
-    'min_max_line_size': [(1,3),(1,3)],
+    'min_max_line_size': [(1, 3), (1, 3)],
     'rotate': True,
     'fades_per_mat': 16,
     'new_background': True,
@@ -13,28 +12,23 @@ matrix_params = {
 }
 
 matrix_lister = MatrixLister(**matrix_params)
-#%%
 model, callbacks = matrix_lister.init_model(32, 128, 'none')  # auto, line, triangle, none
-#%%
 model.load_weights('weights.h5')
-#%%
+
 batch_size = 50
 batch_num = 10
 epochs = 100
 
 generator = matrix_lister.init_generator(batch_size, batch_num)
 
-
 start = time.time()
 hist = model.fit(generator, epochs=epochs, callbacks=callbacks)
 print(time.time() - start)
-#%%
+
 matrix_lister.save_model(model, 'none')  # auto, line, triangle, none
-#%%
 matrix_lister.display_frames(model, 1)
-#%%
+
 ani = matrix_lister.plot_matrices(model, 50, interval=500)
 plt.show()
-#%%
+
 plot_training_history(hist, with_val=False)
-#%%
